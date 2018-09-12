52410b1165dcaefe198bc171e552a378df5341bf
maya <mayanb@gmail.com>
2018-09-12 14:13:44 -0700

    ununiqe

--- api/models.py
+++ api/models.py
@@ -41,7 +41,7 @@ class Product(models.Model):
 class ShopifySKU(models.Model):
 	team = models.ForeignKey(Team, related_name='shopify_skus', on_delete=models.CASCADE)
 	name = models.CharField(max_length=300)
-	variant_id = models.CharField(max_length=50, unique=True)
+	variant_id = models.CharField(max_length=50)
 	variant_sku = models.CharField(max_length=100, null=True)
 	product = models.ForeignKey(Product, related_name='shopify_skus', on_delete=models.CASCADE, null=True)
 	conversion_factor = models.DecimalField(default=1, max_digits=10, decimal_places=6)
